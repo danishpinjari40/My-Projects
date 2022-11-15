57ea25f007c2d2084de5a248edd90f19ffcd32b9
Danish Pinjari <danishpinjari40@gmail.com>
2022-11-15 23:34:37 +0530

    Day 2 Netflix added

--- my_netflix/src/App.js
+++ my_netflix/src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
+import Navbar from './Components/Navbar/Navbar';
 
-import './App.css';
+
+
 
 function App() {
 	return (
-		<div className="App">
-			<h1>This is my NetFlix</h1>
+		<div>
+			<Navbar/>
 		</div>
 	);
 }
