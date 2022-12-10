fe6e40b4774efb74ad4ac8ddbfe315639be0c6e8
Danish Pinjari <danishpinjari40@gmail.com>
2022-12-10 20:56:48 +0530

    New Projects Added

---
+++ My_Portfolio/src/App.js
@@ -0,0 +1,15 @@
+
+import { Box, Heading } from '@chakra-ui/react';
+import './App.css';
+
+function App() {
+  return (
+    < >
+      <Box>
+<Heading>Danish Pinjari</Heading>
+      </Box>
+    </>
+  );
+}
+
+export default App;
